95610c4b0ebe422e86c05d246be58659600a8944
taruna-mandavra <tmandavra@gmail.com>
2026-06-17 00:33:11 +0530

    Implement authentication routes and middleware for user verification

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,8 @@ import { clerkMiddleware } from "@clerk/express";
 import job from "./lib/cron.js";
 import clerkWebhook from "./webhooks/clerk.webhook.js";
 
+import authRoutes from "./routes/auth.route.js";
+
 
 const app = express();
 
@@ -32,6 +34,9 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
+
+
+app.use("/api/auth", authRoutes);
 
 // if the public directory exists, serve the static files
 // this is for the production build
