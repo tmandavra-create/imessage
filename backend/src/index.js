29576e3bdf4c3cfe95357663de5ae7fade983996
taruna-mandavra <tmandavra@gmail.com>
2026-06-17 01:12:31 +0530

    -Implement message controller with functions to get users, conversations and messages -Add route for sending messages with media uploads -Introduce middleware for handling file uploads -Update package.json and package-lock.json to include necessary dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,7 @@ import job from "./lib/cron.js";
 import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 
 const app = express();
@@ -37,6 +38,7 @@ app.get("/health", (req, res) => {
 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // if the public directory exists, serve the static files
 // this is for the production build
