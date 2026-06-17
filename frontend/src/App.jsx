aea0863940c2d241d2f999941abdd5a46f02be50
taruna-mandavra <tmandavra@gmail.com>
2026-06-17 17:29:54 +0530

    -Created AuthPage and ChatPage components for future routing. -Updated index.css to integrate Tailwind CSS and HeroUI styles. -Add axios instance and utility functions ,implement auth store with socket integration. -Implemented hooks for keyboard sound effects and media query handling. -Enhanced chat store with new state management for conversations,messages and user interactions.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,49 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
+import PageLoader from "./components/PageLoader";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
-import './App.css'
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  // option 1
+  // const { checkAuth, isCheckingAuth, clearAuth } = useAuthStore();
+
+  // option 2 - better for performance
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
+
   return (
-    <div>
-      <h1>MY APP</h1>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal"/>
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+        <Toaster />
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 
-export default App
+export default App;
